0cbb37d7ef3746f21ab14d11191513da6b5f42fa
Forkboi1 <m7md123493@gmail.com>
2023-11-26 22:17:11 +0300

    completed retrieveUserInfo function

--- src/main/java/com/bloodbankapp/bloodbankapp/database/DataBase.java
+++ src/main/java/com/bloodbankapp/bloodbankapp/database/DataBase.java
@@ -10,6 +10,7 @@ public class DataBase {
     private final String url = "jdbc:postgresql://ep-shrill-darkness-20221653.us-east-2.aws.neon.tech/blood%20system?user=ayed87&password=2KdnfuWpEa3e&sslmode=require";
     private final String username = "ayed87";
     private final String password = "2KdnfuWpEa3e";
+    private Person currentSystemUser;
     private Connection connection;
     private DataBase() throws SQLException {
         connection = DriverManager.getConnection(url, username, password);
@@ -22,13 +23,21 @@ public class DataBase {
         }
         return  dataBase;
     }
-    public List<String> getUserInfo(String snn) throws Exception{
+    // function retrieve user info if available else it will raise an exception
+    public Person retrieveUserInfo(String snn) throws SQLException{
+        Person user;
         Statement s1 = connection.createStatement();
+        // a query to retrieve the specific info of the current user
         ResultSet r1 = s1.executeQuery("SELECT * " +
                 "FROM person " +
                 "WHERE ssn = " + snn);
-
-        return null;
+//        r1.getString("firstname");
+        if (r1.getString("person_type").equals("Admin")){
+            currentSystemUser = new Admin ();
+        } else if (r1.getString("person_type").equals("system_user")) {
+            currentSystemUser = new SystemUser();
+        }
+        return currentSystemUser;
     }
     public  void method(){
         try {
