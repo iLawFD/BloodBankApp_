e6c4f5a22138a87c7a52391667d185c83dd29129
ayed87 <gake12234@gmail.com>
2023-11-26 21:22:27 +0300

    added person class

--- src/main/java/com/bloodbankapp/bloodbankapp/database/Person.java
+++ src/main/java/com/bloodbankapp/bloodbankapp/database/Person.java
@@ -1,4 +1,69 @@
 package com.bloodbankapp.bloodbankapp.database;
 
 public class Person {
+    private int ssn;
+    private  String firstName;
+    private  String lastName;
+    private String address;
+
+    private String phone_number;
+    private  String email;
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    private  String personType;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPersonType(String personType) {
+        this.personType = personType;
+    }
+
+    public String getPersonType() {
+        return personType;
+    }
 }
